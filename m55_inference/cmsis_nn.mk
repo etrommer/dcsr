79db93657332a45eb513b4fa732cd35194bb2d44
Elias Trommer <elias_trommer@posteo.de>
2023-02-10 16:13:07 +0100

    Link CMSIS-NN as static library
    This links CMSIS-NN as a static library, rather than compiling it
    together with in-tree source files. This allows CMSIS-NN to have a
    separate optimization level and compiler flags as the rest of the code.
    
    Compiling CMSIS-NN with `-O0` fails, which is not helpful when compiling
    the remaining code for debugging.

--- m55_inference/cmsis_nn.mk
+++ m55_inference/cmsis_nn.mk
@@ -1,10 +1,6 @@
 INCLUDE_DIRS += \
-			$(CMSIS_DIR) \
 			$(CMSIS_DIR)/CMSIS/Core/Include \
 			$(CMSIS_DIR)/CMSIS/DSP/Include \
-			$(CMSIS_DIR)/CMSIS/NN/Include \
-
-CSOURCES += \
-			$(shell find $(CMSIS_DIR)/CMSIS/NN/Source -type f -name '*.c') \
+			$(CMSIS_NN_DIR)/Include \
 
 DEFS += -DCMSIS_NN
